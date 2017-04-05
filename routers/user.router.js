f3940c556daeadbf9582228da74b3baee8ecf859
Desimor <moraismatthew@gmail.com>
2017-04-05 00:12:07 -0400

    Spelled Express Wrong

--- routers/user.router.js
+++ routers/user.router.js
@@ -1,4 +1,4 @@
-const express = require('expess');
+const express = require('express');
 const router = express.Router();
 const User = require('../models/user.model');
 const passport = require('passport');

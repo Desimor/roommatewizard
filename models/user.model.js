2449e546e07bff0cf966db811a40175ebe5c60f9
Desimor <moraismatthew@gmail.com>
2017-04-05 20:23:19 -0400

    Created User Model Created a user model file that contains a basic schema for the user and also includes salt and hashing for passwords set by the user.

---
+++ models/user.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const signature = process.env.SIGNATURE || require('../secrets').SIGNATURE;
+
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    salt: {
+        type: String
+    },
+    hash: {
+        type: String
+    }
+});
+
+userSchema.methods.setPassword = function(password){
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64)
+                        .toString('hex');
+}
+userSchema.methods.validPassword = function(passwrod){
+    const hash = crypto.pbkdf2Sync(passwrod, this.salt, 1000, 64)
+                        .toString('hex');
+    return this.has === hash;
+}
+userSchema.methods.generateJwt = function(){
+    const expiration = new Date();
+    expiration.setDate(expiration.getDate() + 7);
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        name: this.name,
+        exp: parseInt(expiration.getTime() / 1000)
+    }, signature);
+}
+
+var User = mongoose.model('User', userSchema);
+module.exports = User;
